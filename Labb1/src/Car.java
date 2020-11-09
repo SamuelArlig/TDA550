a73c1f197deab4c567e657ba7090d45460223db7
Samuel Ärlig <samuel.arlig@gmail.com>
2020-11-09 15:58:59 +0100

    Adds Javadoc comments to Car

--- Labb1/src/Car.java
+++ Labb1/src/Car.java
@@ -12,49 +12,107 @@ public abstract class Car implements Movable {
     protected double angle = 0; // Radians
     protected double turnSpeed; // Radians
 
+    /**
+     * Returns the number of doors
+     *
+     * @return the number of doors on the car
+     */
     public int getNrDoors(){
         return nrDoors;
     }
+    /**
+     * Returns the cars engine power
+     *
+     * @return the cars engine power
+     */
     public double getEnginePower(){
         return enginePower;
     }
 
+    /**
+     * Returns the cars current speed
+     *
+     * @return the cars current speed
+     */
     public double getCurrentSpeed() {
         return currentSpeed;
     }
 
+    /**
+     * Returns the color of the car
+     *
+     * @return the color of the car
+     */
     public Color getColor(){
         return color;
     }
 
+    /**
+     * Set the color of the car
+     * @param clr the new {@code Color} for the car
+     */
     public void setColor(Color clr){
         color = clr;
     }
 
+    /**
+     * Starts the engine of the car.
+     *
+     */
     public void startEngine(){
         currentSpeed = 0.1;
     }
 
+    /**
+     * Stops the engine of the car
+     */
     public void stopEngine(){
         currentSpeed = 0;
     }
 
+    /**
+     * Return the speed factor of the car
+     * @return the speed factor of the car
+     */
     protected abstract double speedFactor();
 
+    /**
+     * Increases the speed of the car by a factor amount
+     * Can't increase speed over enginePower
+     * @param amount amount to increase the speed, must be > 0
+     */
     private void incrementSpeed(double amount){
         amount = Math.max(0, amount);
         currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
     }
+
+    /**
+     * Decreases the speed of the car by amount * speedFactor
+     * Can't decrease speed below 0
+     * @param amount amount to decrease the speed, must be > 0
+     */
 
     private void decrementSpeed(double amount){
         amount = Math.max(0, amount);
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
     }
 
+    /**
+     * Increases the speed of the car
+     *
+     * @param amount amount to increment speed, must be between 0 and 1.
+     */
+
     public void gas(double amount){
         amount = Math.max(0, Math.min(amount, 1));
         incrementSpeed(amount);
     }
+
+    /**
+     * Decrease the speed of the car
+     *
+     * @param amount amount to decrement speed, must be between 0 and 1.
+     */
 
     public void brake(double amount){
         amount = Math.max(0, Math.min(amount, 1));
