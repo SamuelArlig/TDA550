dbb78ae69bb2dee34d095e716cf4b5e37cf56fe0
Samuel Ärlig <samuel.arlig@gmail.com>
2020-11-11 16:03:02 +0100

    Adds car race

---
+++ Labb1/src/Race.java
@@ -0,0 +1,39 @@
+
+
+public class Race {
+    /**
+     * Run a race between a Saab95 with turbo and a Volvo240
+     *
+     * @param args Don't matter
+     */
+    public static void main(String[] args) {
+        Saab95 saab = new Saab95();
+
+        Car[] cars = new Car[2];
+        cars[0] = saab;
+        cars[1] = new Volvo240();
+
+        for(Car car : cars){
+            car.startEngine();
+        }
+
+        for(int i = 0; i < 10; i++){
+            if(saab.getCurrentSpeed() > 5){
+                saab.setTurboOn();
+            }
+
+            for(Car car : cars){
+                car.gas(1);
+                car.move();
+
+                if(car.x > 50){
+                    car.turnLeft();
+                }
+
+                System.out.println(car.modelName + ": x:" + car.x + ", y: " + car.y + ", Speed: " + car.getCurrentSpeed());
+            }
+        }
+
+    }
+}
+
